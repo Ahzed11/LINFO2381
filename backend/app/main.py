28b176fd96dee90f2a626fbfe38dd9a664e52ccd
Alexandre Zenon <a.zenon@student.uclouvain.be>
2024-04-25 17:19:22 +0200

    basic patient creation example

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 from .libs.CouchDBClient import CouchDBClient
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field
+
+class Patient(BaseModel):
+    first_name: str
+    last_name: str
 
 app = FastAPI()
 couchdb_client = CouchDBClient(url="http://couchdb:5984")
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        couchdb_client.createDatabase("patients")
+    except Exception:
+        pass
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/patients/")
+async def create_patient(patient: Patient):
+    patient_as_json = jsonable_encoder(patient)
+    key = couchdb_client.addDocument("patients", patient_as_json) 
+    new_patient = couchdb_client.getDocument("patients", key)
+    return {"message": f"Hello {new_patient}"}
